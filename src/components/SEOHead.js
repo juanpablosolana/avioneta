ae150d2c476d0dc21bc165ebdac6867f99577e68
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2023-04-03 10:50:46 -0600

    📚 Feature:SEO component

---
+++ src/components/SEOHead.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+function SEOHead ({ title, description, keywords }) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link rel='icon' href='/favicon.ico' />
+      <meta name='keywords' content={keywords} />
+    </Head>
+  )
+}
+
+export default SEOHead
