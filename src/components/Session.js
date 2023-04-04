736936c5bfc0b9a4eea742a0c93b91201dccb662
Juan Pablo Solana Ortiz <pablosolana@gmail.com>
2023-04-04 12:13:16 -0600

    🚑 Fix: Update home page buttons

--- src/components/Session.js
+++ src/components/Session.js
@@ -15,17 +15,17 @@ function Signin () {
 function Signout () {
   const router = useRouter()
   return (
-    <div className='flex flex-1'>
+    <div>
       <button
         onClick={() => router.push('/profile')}
-        className='w-full px-4 py-2 text-xl font-semibold text-white rounded-lg shadow-lg bg-sky-900'
+        className='px-4 py-2 mx-5 text-xl font-bold text-white rounded-lg shadow-lg bg-sky-900'
       >
         Ver perfil
       </button>
 
       <button
         button onClick={() => signOut()}
-        className='w-full px-4 py-2 text-xl font-semibold text-white rounded-lg shadow-lg bg-sky-900'
+        className='px-4 py-2 text-xl font-bold text-white rounded-lg shadow-lg bg-sky-900'
       >
         Salir
       </button>
